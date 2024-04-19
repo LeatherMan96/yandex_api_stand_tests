69f831454eb0f5608ca5f9ffcffe98334cebf8f0
LeatherMan6969 <unface4@gmail.com>
2024-04-19 14:28:30 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://ebf1a899-980b-4f54-964c-97ff18853d64.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
